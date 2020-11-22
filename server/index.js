1a5d5b23e56b0d61be04bf2e6e6d3a23e00ae866
igruntplay <gaspar.onesto@gmail.com>
2020-11-22 01:14:44 -0300

    removed all the DB mongo methods

--- server/index.js
+++ server/index.js
@@ -4,38 +4,16 @@ const app = express()
 const path = require("path");
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const db = require('./userDB.js');
-const dbPost = require('./postDB.js');
-const { register } = require('./userDB.js');
 const session = require('express-session');
 const { debug } = require('console');
 const multer = require('multer');
 
-/** 
-const storage = multer.diskStorage({
-    destination: '/public/img/imgPost',
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-
-    }
-});
-app.use(multer({
-    dest: '/public/img/imgPost',
-    storage
-}).single('postPic'));
-const upload = multer({
-    storage
-});
-*/
 app.set('views', path.join(__dirname, "s"))
 
 // set cors policy
 app.use(cors());
 // path public
 app.use(express.static(path.join(__dirname, 'public')));
-// parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
 
 // session key
 app.use(session({
@@ -59,12 +37,7 @@ app.get('/', function (req, res) {
 // home endpoint..
 app.get('/home', function (req, res) {
     if (req.session.loggedUser) {
-        dbPost.showPost((allPosts) => {
-            allPosts.map(element => {
-                element.path = `/exp/${element.Section}/${element._id}`;
-            });
 
-        });
     }
     else { res.redirect('/'); }
 })
@@ -76,45 +49,13 @@ app.post('/register', function (req, res) {
         res.status(400).send("No se recibieron bien los datos");
         return;
     } else {
-        db.register(req.body.newPW, (bool, user) => {
-            if (bool) {
-                req.session.loggedUser = user;
-                return res.status(200).json({
-                    success: true,
-                    redirect: "/home",
-                    userid: req.session.loggedUser.userid
-                    //  points: req.body.points
-                });
-            }
-            else {
-                req.session.message = {
-                    success: false,
-                    text: "Error al registrarse intentelo de nuevo"
-                };
-                res.redirect('/');
-            }
-        });
 
-        //  res.status(200)
+
     }
 })
 // login post..
 app.post('/login', function (req, res) {
-    db.login(req.body.userid, req.body.password, result => {
-        if (result) {
-            req.session.loggedUser = result;
-            return res.status(200).json({
-                success: true,
-                redirect: "/home"
-            });
-        } else {
-            req.session.message = {
-                class: "failure",
-                text: "No se pudo iniciar la sesion"
-            };
-            res.redirect('/');
-        }
-    });
+
 
 });
 
@@ -126,19 +67,7 @@ app.post('/exp', function (req, res) {
         if (req.session.loggedUser) {
             req.body.userid = req.session.loggedUser.userid;
             req.body.rank = req.session.loggedUser.rank;
-            dbPost.createPost(req.body, (bool, postid) => {
-                if (bool) {
-                    res.status(200).json({
-                        success: true,
-                        redirect: `/exp/${req.body.Section}/${postid}`
-                    });
-                } else {
-                    req.session.message = {
-                        class: "failure",
-                        text: "No se pudo iniciar la sesion"
-                    };
-                }
-            });
+
         } else { res.redirect('/'); }
 
     }
@@ -149,27 +78,11 @@ app.get('/exp/:section/:postid?', function (req, res) {
     // if the section and the post id EXISTS render it..
     if (req.session.loggedUser) {
         if (req.params.section && req.params.postid) {
-            return dbPost.getPost(req.params.postid, (postid, bool) => {
-                console.log(postid.result.Section);
 
-
-            })
         }
 
         if (req.params.section) {
-            return dbPost.filterPost(req.params.section, (bool, postArray) => {
-                if (bool) {
-                    postArray.map(element => {
-                        element.path = `/exp/${element.Section}/${element._id}`;
-                    });
 
-                } else {
-                    req.session.message = {
-                        class: "failure",
-                        text: "No se pudo llegar"
-                    };
-                }
-            });
         }
     } else {
         res.redirect('/');
@@ -183,9 +96,7 @@ app.post('/exp/:section/:postid?'), function (req, res) {
     } else {
         if (req.session.loggedUser) {
             console.log(req.body);
-            dbPost.comment(req.body.commentTXT, (bool, comment)=>{
 
-            });
 
         } else {
             req.session.message = {
@@ -196,22 +107,7 @@ app.post('/exp/:section/:postid?'), function (req, res) {
     }
 }
 
-/** 
-// doesnt work yet..
-app.post('/imgupload', function (req, res, cb) {
-    upload(req, res, (err) => {
-        if (err) { res.redirect('/home'); }
-        else {
-            console.log(req.file);
-            res.redirect('/home');
-        }
-    });
-    console.log(req.file);
-    res.send('Archivo subido');
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
-})
-*/
+
 
 // heroku port access..
 app.set('port', process.env.PORT || 3000);
