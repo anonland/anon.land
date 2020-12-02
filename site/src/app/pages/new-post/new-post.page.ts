607d2969615cc0a115af5d1b2407cc8f1ef4eea9
martnpz <martinmatiasperez2015@gmail.com>
2020-12-02 17:17:07 -0300

    -Add logo -add new color variables -modified new post modal -add anon icons

--- site/src/app/pages/new-post/new-post.page.ts
+++ site/src/app/pages/new-post/new-post.page.ts
@@ -1,34 +1,54 @@
-import { HttpClient } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { ModalController } from "@ionic/angular";
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
 
 @Component({
-  selector: "app-new-post",
-  templateUrl: "./new-post.page.html",
-  styleUrls: ["./new-post.page.scss"],
+  selector: 'app-new-post',
+  templateUrl: './new-post.page.html',
+  styleUrls: ['./new-post.page.scss'],
 })
 export class NewPostPage implements OnInit {
   public newPostForm: FormGroup;
 
   constructor(private modalCtrl: ModalController, private http: HttpClient) {
     this.newPostForm = new FormGroup({
-      category: new FormControl("general", Validators.required),
-      img: new FormControl(""),
+      category: new FormControl('general', Validators.required),
+      img: new FormControl(''),
       title: new FormControl(undefined, Validators.required),
       body: new FormControl(undefined),
     });
   }
 
+  // Image preview container.
+  img: any = '';
+
+  // Text on Img upload input.
+  showUploadImgText = true;
+
   ngOnInit() { }
 
-  cancel() {
+  cancelCreate() {
     this.modalCtrl.dismiss();
   }
 
-  create() {
+  createNewPost() {
     const { category, img, title, body } = this.newPostForm.value;
 
-    this.modalCtrl.dismiss({ category, img, title, body, opid: localStorage.getItem('session') });
+    this.modalCtrl.dismiss({ category, img, title, body });
+  }
+
+  onSelectFile(preview: any) {
+    if (preview.target.files && preview.target.files[0]) {
+      const reader = new FileReader();
+
+      this.showUploadImgText = false;
+
+      reader.readAsDataURL(preview.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.img = event.target.result;
+      };
+    }
   }
 }
